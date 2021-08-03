13bbe32bc550ccdda68b3e4c76b9de66f3228c0a
abstrakt <abstrakt.osu@gmail.com>
2021-08-03 19:29:38 +0200

    Add desktop, desktop-frontend with nx-electron

---
+++ apps/desktop-frontend/src/app/app.tsx
@@ -0,0 +1,10 @@
+import styles from "./app.module.css";
+
+import { ReactComponent as Logo } from "./logo.svg";
+import star from "./star.svg";
+
+export function App() {
+  return <div className={styles.app}>Test</div>;
+}
+
+export default App;
