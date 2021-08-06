650831b78f58e50837be322fabb90cb3d0a4b10f
abstrakt <abstrakt.osu@gmail.com>
2021-08-06 20:07:11 +0200

    cursor

--- libs/feature-replay-viewer/src/pixi/PlayfieldContainer.ts
+++ libs/feature-replay-viewer/src/pixi/PlayfieldContainer.ts
@@ -49,94 +49,100 @@ export class PlayfieldContainer {
     );
   }
 
-  // prepareOsuCursor(time: number) {
-  //   const cursor = new OsuClassicCursor();
-  //   const { showTrail, scaleWithCS, scale } = this.context.view.osuCursor;
-  //
-  //   const pi = findIndexInReplayAtTime(this.replayFrames, time);
-  //   // we are either before the beginning or after the end of the replay
-  //   if (pi === -1 || pi + 1 >= this.replayFrames.length) return;
-  //
-  //   // console.log(pi);
-  //   const position = interpolateReplayPosition(this.replayFrames[pi], this.replayFrames[pi + 1], time);
-  //   const trailPositions = [];
-  //   if (showTrail) {
-  //     for (let i = 1; i <= 5 && pi - i >= 0; i++) {
-  //       trailPositions.push(this.replayFrames[pi - i].position);
-  //     }
-  //   }
-  //
-  //   cursor.prepare({
-  //     position,
-  //     trailPositions,
-  //     cursorScale: scale,
-  //     cursorTexture: this.skin.getTexture(SkinTextures.CURSOR),
-  //     cursorTrailTexture: this.skin.getTexture(SkinTextures.CURSOR_TRAIL),
-  //   });
-  //
-  //   this.cursorContainer.addChild(cursor.container);
-  // }
-  //
-  // prepareAnalysisCursor(time: number) {
-  //   const cursor = new AnalysisCursor();
-  //   const pi = findIndexInReplayAtTime(this.replayFrames, time);
-  //   // we are either before the beginning or after the end of the replay
-  //   if (pi === -1 || pi + 1 >= this.replayFrames.length) return;
-  //   const position = interpolateReplayPosition(this.replayFrames[pi], this.replayFrames[pi + 1], time);
-  //
-  //   const interesting = [];
-  //   const masks: number[] = [];
-  //   // the interesting rule can be changed...
-  //
-  //   const points = [];
-  //   const trailCount = 25;
-  //   for (let i = trailCount - 1; i >= 0; i--) {
-  //     masks[i] = 0;
-  //     if (pi - i >= 0) {
-  //       const r = this.replayFrames[pi - i];
-  //       if (r.actions.includes(OsuAction.leftButton)) masks[i] |= 1;
-  //       if (r.actions.includes(OsuAction.rightButton)) masks[i] |= 2;
-  //       if (i + 1 === trailCount) continue;
-  //
-  //       // i has a bit that i + 1 does not have (bit=press)
-  //       if ((masks[i] ^ masks[i + 1]) & masks[i]) {
-  //         interesting[i] = true;
-  //       }
-  //     }
-  //   }
-  //   const colorScheme = [
-  //     0x5d6463, // none gray
-  //     0xffa500, // left (orange)
-  //     0x00ff00, // right (green)
-  //     // 0xfa0cd9, // right (pink)
-  //     0x3cbdc1, // both (cyan)
-  //   ];
-  //   for (let i = 0; i < trailCount && pi - i >= 0; i++) {
-  //     const j = pi - i;
-  //     const maskDelta = (masks[i] ^ masks[i + 1]) & masks[i]; //
-  //     points.push({
-  //       interesting: interesting[i],
-  //       color: colorScheme[maskDelta],
-  //       position: this.replayFrames[j].position,
-  //     });
-  //   }
-  //   cursor.prepare({ points, smoothedPosition: position });
-  //   cursor.container.position.set(position.x, position.y);
-  //
-  //   this.cursorContainer.addChild(cursor.container);
-  // }
-  //
-  // prepareCursor(time: number) {
-  //   this.cursorContainer.removeChildren();
-  //
-  //   if (this.view.osuCursor.enabled) {
-  //     this.prepareOsuCursor(time);
-  //   }
-  //   if (this.view.analysisCursor.enabled) {
-  //     this.prepareAnalysisCursor(time);
-  //   }
-  // }
-  //
+  prepareOsuCursor(scene: Scene) {
+    const { time, replay, skin } = scene;
+    const cursor = new OsuClassicCursor();
+    const { showTrail, scaleWithCS, scale } = scene.view.osuCursor;
+    if (!replay) return;
+    const frames = replay.frames;
+
+    const pi = findIndexInReplayAtTime(frames, time);
+    // we are either before the beginning or after the end of the replay
+    if (pi === -1 || pi + 1 >= frames.length) return;
+
+    // console.log(pi);
+    const position = interpolateReplayPosition(frames[pi], frames[pi + 1], time);
+    const trailPositions = [];
+    if (showTrail) {
+      for (let i = 1; i <= 5 && pi - i >= 0; i++) {
+        trailPositions.push(frames[pi - i].position);
+      }
+    }
+
+    cursor.prepare({
+      position,
+      trailPositions,
+      cursorScale: scale,
+      cursorTexture: skin.getTexture(SkinTextures.CURSOR),
+      cursorTrailTexture: skin.getTexture(SkinTextures.CURSOR_TRAIL),
+    });
+
+    this.cursorContainer.addChild(cursor.container);
+  }
+
+  prepareAnalysisCursor(scene: Scene) {
+    const { replay, time } = scene;
+    if (!replay) return;
+    const { frames } = replay;
+    const cursor = new AnalysisCursor();
+    const pi = findIndexInReplayAtTime(frames, time);
+    // we are either before the beginning or after the end of the replay
+    if (pi === -1 || pi + 1 >= frames.length) return;
+    const position = interpolateReplayPosition(frames[pi], frames[pi + 1], time);
+
+    const interesting = [];
+    const masks: number[] = [];
+    // the interesting rule can be changed...
+
+    const points = [];
+    const trailCount = 25;
+    for (let i = trailCount - 1; i >= 0; i--) {
+      masks[i] = 0;
+      if (pi - i >= 0) {
+        const r = frames[pi - i];
+        if (r.actions.includes(OsuAction.leftButton)) masks[i] |= 1;
+        if (r.actions.includes(OsuAction.rightButton)) masks[i] |= 2;
+        if (i + 1 === trailCount) continue;
+
+        // i has a bit that i + 1 does not have (bit=press)
+        if ((masks[i] ^ masks[i + 1]) & masks[i]) {
+          interesting[i] = true;
+        }
+      }
+    }
+    const colorScheme = [
+      0x5d6463, // none gray
+      0xffa500, // left (orange)
+      0x00ff00, // right (green)
+      // 0xfa0cd9, // right (pink)
+      0x3cbdc1, // both (cyan)
+    ];
+    for (let i = 0; i < trailCount && pi - i >= 0; i++) {
+      const j = pi - i;
+      const maskDelta = (masks[i] ^ masks[i + 1]) & masks[i]; //
+      points.push({
+        interesting: interesting[i],
+        color: colorScheme[maskDelta],
+        position: frames[j].position,
+      });
+    }
+    cursor.prepare({ points, smoothedPosition: position });
+    cursor.container.position.set(position.x, position.y);
+
+    this.cursorContainer.addChild(cursor.container);
+  }
+
+  prepareCursors(scene: Scene) {
+    this.cursorContainer.removeChildren();
+
+    if (scene.view.osuCursor.enabled) {
+      this.prepareOsuCursor(scene);
+    }
+    if (scene.view.analysisCursor.enabled) {
+      this.prepareAnalysisCursor(scene);
+    }
+  }
+
   // private static texturesForJudgement(t: MainHitObjectVerdict, lastInComboSet?: boolean) {
   //   switch (t) {
   //     case "GREAT":
@@ -176,11 +182,8 @@ export class PlayfieldContainer {
   // }
 
   prepare(scene: Scene) {
-    const { skin, beatmap, gameplayInfo, time, replay } = scene;
-
     this.hitObjectPreparer.prepare(scene);
-
-    // this.prepareCursor(time);
+    this.prepareCursors(scene);
     //
     // this.prepareJudgements(time);
   }
