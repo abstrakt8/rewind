ee3c8ad42a98e31d1292aeb074dd7fb1987b54fc
abstrakt <abstrakt.osu@gmail.com>
2021-08-29 19:43:41 +0200

    Set environment API url accordingly

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,9 +2,10 @@ import { TheaterProvider } from "@rewind/feature-replay-viewer";
 import { StrictMode } from "react";
 import * as ReactDOM from "react-dom";
 import { WebTestApp } from "./app/webTestApp";
+import { environment } from "./environments/environment";
 
 // TODO: process.env.URL
-const url = "http://localhost:7271";
+const url = environment.url;
 
 ReactDOM.render(
   <StrictMode>
