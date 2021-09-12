3cd0c574c3bca72aa09fe40065b755e6b9ea334d
abstrakt <abstrakt.osu@gmail.com>
2021-09-12 16:57:41 +0200

    Refactor frontend-preload-api

--- libs/electron/api/src/index.ts
+++ libs/electron/api/src/index.ts
@@ -16,6 +16,13 @@ export interface SecureElectronAPI {
   receive: (channel: ValidReceiveChannel, func: (...args: any[]) => void) => Destroyer;
 }
 
+export interface FrontendPreloadAPI {
+  getAppVersion: () => Promise<string>;
+  getPlatform: () => Promise<string>; // actually NodeJS.Platform
+  selectDirectory: (defaultPath: string) => Promise<string | null>;
+  reboot: () => void;
+}
+
 type PathType = "appData" | "userData" | "appResources" | "logs";
 
 export interface BackendPreloadAPI {
