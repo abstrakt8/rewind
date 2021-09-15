b5d8b007d3c103e22dcfe73451e47526ca9c6f6d
abstrakt <abstrakt.osu@gmail.com>
2021-09-15 18:16:21 +0200

    OsuDefaultSkin will be downloaded as well in the build

--- tools/downloadDefaultSkin.js
+++ tools/downloadDefaultSkin.js
@@ -2,22 +2,34 @@ const request = require("superagent");
 const fs = require("fs");
 const AdmZip = require("adm-zip");
 
-const source = "https://drive.google.com/uc?id=1ftm6PDXGoe1Q3dlyZgejC4nnWKI21ZXd&export=download";
-const zipFile = "RewindDefaultSkin.zip";
+const sources = [
+  {
+    url: "https://drive.google.com/uc?id=1tNHWSBlfNZp2h1C_LRyjddRd9AZhJj6p&export=download",
+    name: "OsuDefaultSkin",
+  },
+  {
+    url: "https://drive.google.com/uc?id=1ftm6PDXGoe1Q3dlyZgejC4nnWKI21ZXd&export=download",
+    name: "RewindDefaultSkin",
+  },
+];
 
-request
-  .get(source)
-  .on("error", (error) => {
-    console.log(error);
-    process.exit(1);
-  })
-  .pipe(fs.createWriteStream(zipFile))
-  .on("finish", () => {
-    console.log("Finished downloading .zip file");
-    const zip = new AdmZip(zipFile);
-    console.log("Starting to unzip the skin");
-    zip.extractAllTo("resources/Skins", false);
-    console.log("Unzipped the skin");
-    fs.rmSync(zipFile);
-    console.log("Cleanup done");
-  });
+for (const source of sources) {
+  const { url, name } = source;
+  const zipFileName = `${name}.zip`;
+  request
+    .get(url)
+    .on("error", (error) => {
+      console.log(`Error downloading ${name}: ` + error);
+      process.exit(1);
+    })
+    .pipe(fs.createWriteStream(zipFileName))
+    .on("finish", () => {
+      console.log("Finished downloading .zip file");
+      const zip = new AdmZip(zipFileName);
+      console.log("Starting to unzip the skin");
+      zip.extractAllTo("resources/Skins", false);
+      console.log("Unzipped the skin");
+      fs.rmSync(zipFileName);
+      console.log("Cleanup done");
+    });
+}
