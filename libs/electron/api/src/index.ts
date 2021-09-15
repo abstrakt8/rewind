8799836d5fedbc63a518d6c35ac264928b32d1a0
abstrakt <abstrakt.osu@gmail.com>
2021-09-15 11:41:34 +0200

    Remove unnecessary stuff

--- libs/electron/api/src/index.ts
+++ libs/electron/api/src/index.ts
@@ -1,21 +1,3 @@
-export interface ElectronAPI {
-  getAppVersion: () => Promise<string>;
-  platform: string;
-}
-
-export const validSendChannels = ["openDirectorySelect", "reboot"] as const;
-export type ValidSendChannel = typeof validSendChannels[number];
-
-export const validReceiveChannels = ["directorySelected"] as const;
-export type ValidReceiveChannel = typeof validReceiveChannels[number];
-
-type Destroyer = () => void;
-
-export interface SecureElectronAPI {
-  send: (channel: ValidSendChannel, ...args: string[]) => void;
-  receive: (channel: ValidReceiveChannel, func: (...args: any[]) => void) => Destroyer;
-}
-
 export interface FrontendPreloadAPI {
   getAppVersion: () => Promise<string>;
   getPlatform: () => Promise<string>; // actually NodeJS.Platform
