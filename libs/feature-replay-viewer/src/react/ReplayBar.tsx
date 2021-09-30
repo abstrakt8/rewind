11462609f18f67c65bcbbc6bfe8b177d3fe7f127
abstrakt <abstrakt.osu@gmail.com>
2021-09-30 23:08:45 +0200

    Slider included in ReplayBar

--- libs/feature-replay-viewer/src/react/ReplayBar.tsx
+++ libs/feature-replay-viewer/src/react/ReplayBar.tsx
@@ -1,4 +1,5 @@
-import { Box, darken, Stack, Tooltip } from "@mui/material";
+import { Box, darken, Slider, Stack, Tooltip } from "@mui/material";
+import { formatGameTime } from "@rewind/osu/math";
 
 interface EventLineProps {
   color: string;
@@ -12,7 +13,7 @@ const EventLine = ({ color, tooltip, positions }: EventLineProps) => {
       <div>
         <Box
           sx={{
-            height: "0.5em",
+            height: "14px",
             backgroundColor: darken(color, 0.6),
             "&:hover": {
               backgroundColor: darken(color, 0.4),
@@ -46,12 +47,54 @@ const EventLine = ({ color, tooltip, positions }: EventLineProps) => {
 export interface ReplayBarProps {}
 
 export function ReplayBar(props: ReplayBarProps) {
+  const valueLabelFormat = (value: number) => {
+    return formatGameTime(value);
+  };
   return (
-    <Stack sx={{ overflow: "hidden" }}>
-      <EventLine color={"hsl(0,100%,50%)"} tooltip={"Misses"} positions={[0.3]} />
-      <EventLine color={"hsl(21,100%,50%)"} tooltip={"Slider Breaks"} positions={[0.0, 0.5, 0.9]} />
-      <EventLine color={"hsl(52,100%,50%)"} tooltip={"50s"} positions={[0.4, 0.6, 0.88]} />
-      <EventLine color={"hsl(96,100%,50%)"} tooltip={"100s"} positions={[0.1, 0.2, 0.33, 0.34, 0.345, 0.6, 0.601]} />
-    </Stack>
+    <Box position={"relative"}>
+      <Stack sx={{ overflow: "hidden", visibility: "visible" }}>
+        <EventLine color={"hsl(0,100%,50%)"} tooltip={"Misses"} positions={[0.3]} />
+        <EventLine color={"hsl(21,100%,50%)"} tooltip={"Slider Breaks"} positions={[0.0, 0.5, 0.9]} />
+        <EventLine color={"hsl(52,100%,50%)"} tooltip={"50s"} positions={[0.4, 0.6, 0.88]} />
+        <EventLine color={"hsl(96,100%,50%)"} tooltip={"100s"} positions={[0.1, 0.2, 0.33, 0.34, 0.345, 0.6, 0.601]} />
+      </Stack>
+      <Slider
+        size={"small"}
+        // The padding here determines how clickable the slider is
+        // This is copied from: https://mui.com/components/slider/#music-player
+        sx={{
+          position: "absolute",
+          top: "50%",
+          transform: "translate(0, -50%)",
+          padding: "2px 0",
+
+          color: "white",
+          height: 6,
+          "& .MuiSlider-thumb": {
+            width: 8,
+            height: 8,
+            transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
+            "&:before": {
+              boxShadow: "0 2px 12px 0 rgba(0,0,0,0.4)",
+            },
+            "&:hover, &.Mui-focusVisible": {
+              boxShadow: `0px 0px 0px 8px ${"rgb(255 255 255 / 16%)"}`,
+            },
+            "&.Mui-active": {
+              width: 20,
+              height: 20,
+            },
+          },
+          "& .MuiSlider-rail": {
+            opacity: 0.6,
+          },
+        }}
+        defaultValue={0.4}
+        getAriaValueText={valueLabelFormat}
+        valueLabelFormat={valueLabelFormat}
+        valueLabelDisplay={"auto"}
+        max={727 * 1000}
+      />
+    </Box>
   );
 }
