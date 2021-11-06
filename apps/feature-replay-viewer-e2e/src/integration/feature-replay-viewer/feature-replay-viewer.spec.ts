4992842ad85bd6b3b51566f6129362d13ea2eca6
abstrakt <abstrakt.osu@gmail.com>
2021-11-06 22:42:01 +0100

    Remove unnecessary files and comments

--- apps/feature-replay-viewer-e2e/src/integration/feature-replay-viewer/feature-replay-viewer.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('feature-replay-viewer: FeatureReplayViewer component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=featurereplayviewer--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to feature-replay-viewer!');
-    });
-});
