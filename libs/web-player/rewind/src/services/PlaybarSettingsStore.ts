2c9f1c046cedc3e7f4145d13cd04768b59ddac08
abstrakt <abstrakt.osu@gmail.com>
2021-12-23 16:54:20 +0100

    Fix default value of `PlaybarSettingsStore`

--- libs/web-player/rewind/src/services/PlaybarSettingsStore.ts
+++ libs/web-player/rewind/src/services/PlaybarSettingsStore.ts
@@ -1,6 +1,10 @@
 import { AbstractSettingsStore } from "./AbstractSettingsStore";
-import { PlaybarSettings } from "../settings/PlaybarSettings";
+import { DEFAULT_PLAY_BAR_SETTINGS, PlaybarSettings } from "../settings/PlaybarSettings";
 import { injectable } from "inversify";
 
 @injectable()
-export class PlaybarSettingsStore extends AbstractSettingsStore<PlaybarSettings> {}
+export class PlaybarSettingsStore extends AbstractSettingsStore<PlaybarSettings> {
+  constructor() {
+    super(DEFAULT_PLAY_BAR_SETTINGS);
+  }
+}
